884d1b1677dbe766448713d3237387ba4130a676
gurpreetSingh4 <gurpreetyarasingh@gmail.com>
2024-04-03 23:24:35 +0530

    style

--- index.js
+++ index.js
@@ -91,9 +91,9 @@ function renderWeatherInfo(weatherInfo){
     desc.innerText = weatherInfo?.weather?.[0]?.description;
     weatherIcon.src = `https://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`; 
     temp.innerText = `${weatherInfo?.main?.temp} °C `;
-    windSpeed.innerText = weatherInfo?.wind?.speed;
-    humidity.innerText = weatherInfo?.main?.humidity;
-    cloudiness.innerText = weatherInfo?.clouds?.all;
+    windSpeed.innerText = `${weatherInfo?.wind?.speed} m/s`;
+    humidity.innerText = `${weatherInfo?.main?.humidity} % `;
+    cloudiness.innerText = `${weatherInfo?.clouds?.all} %`;
 
 }
 
