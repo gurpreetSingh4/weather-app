05eb89c1a768d6571eee4dc88dbd60da64142468
gurpreetSingh4 <gurpreetyarasingh@gmail.com>
2024-04-03 16:12:09 +0530

    working on style

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ function renderWeatherInfo(weatherInfo){
     countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`; 
     desc.innerText = weatherInfo?.weather?.[0]?.description;
     weatherIcon.src = `https://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`; 
-    temp.innerText = weatherInfo?.main?.temp;
+    temp.innerText = `${weatherInfo?.main?.temp} °C `;
     windSpeed.innerText = weatherInfo?.wind?.speed;
     humidity.innerText = weatherInfo?.main?.humidity;
     cloudiness.innerText = weatherInfo?.clouds?.all;
